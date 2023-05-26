faa5b12b4e8703168634b5f49bd2d26e493f7eef
Ansh <varshneyansh321@gmail.com>
2023-05-26 18:56:10 +0530

    initial

---
+++ dummyData.mjs
@@ -0,0 +1,227 @@
+const Info = [
+  {
+    poster: "https://imgs.search.brave.com/6usxrPPEcy2QzghQoMwkE8keSkqmMMegkfBU_C9Hl9o/rs:fit:440:660:1/g:ce/aHR0cHM6Ly93d3cu/dGhlbW92aWVkYi5v/cmcvdC9wL3c0NDBf/YW5kX2g2NjBfZmFj/ZS83VmlRcWxXNVdS/dWxjbkpBZXJjaXpt/QlZ3M3IuanBn",
+    title: "The Peripheral",
+    type: "TV Series",
+    releaseYear: "2022",
+    episodes: 15,
+    tags: ["Darma", "Mystery", "Sci-Fi"],
+    smallDesc:
+      "Set in the future when technology has subtly altered society, a woman discovers a secret connection to an alternate reality as well as a dark future of her own.",
+    creator: "Scott B. Smith",
+    cast: ["Chloe Grace Moretz", "Gary Carr", "Jack Reynor"],
+    rating: "4.5 / 5",
+
+    fullDesc:
+      "Stuck in a small Appalachian town, a young woman's only escape from the daily grind is playing advanced video games. She is such a good player that a company sends her a new video game system to test...but it has a surprise in store. It unlocks all of her dreams of finding a purpose, romance, and glamour in what seems like a game...but it also puts her and her family in real danger.",
+
+    fullDescBy: "—Wilem MacLeod",
+    fullReleaseDate: "October 21, 2022",
+    countryOfOrigin: "United States",
+    Language: "English",
+    filmingLocation: ["Asheville", "North California", "USA"],
+    productionCompanies: [
+      "Amazon Studios",
+      "Big Indie Pictures",
+      "Kilter Films",
+    ],
+
+    cast: [
+      { actorName: "Chloe Grace Moretz", role: "Flynne Fisher" },
+      { actorName: "Gary Carr", role: "Wild Netherton" },
+    ],
+  },
+  {
+    poster: "https://imgs.search.brave.com/6usxrPPEcy2QzghQoMwkE8keSkqmMMegkfBU_C9Hl9o/rs:fit:440:660:1/g:ce/aHR0cHM6Ly93d3cu/dGhlbW92aWVkYi5v/cmcvdC9wL3c0NDBf/YW5kX2g2NjBfZmFj/ZS83VmlRcWxXNVdS/dWxjbkpBZXJjaXpt/QlZ3M3IuanBn",
+    title: "The Peripheral",
+    type: "TV Series",
+    releaseYear: "2022",
+    episodes: 15,
+    tags: ["Darma", "Mystery", "Sci-Fi"],
+    smallDesc:
+      "Set in the future when technology has subtly altered society, a woman discovers a secret connection to an alternate reality as well as a dark future of her own.",
+    creator: "Scott B. Smith",
+    cast: ["Chloe Grace Moretz", "Gary Carr", "Jack Reynor"],
+    rating: "4.5 / 5",
+
+    fullDesc:
+      "Stuck in a small Appalachian town, a young woman's only escape from the daily grind is playing advanced video games. She is such a good player that a company sends her a new video game system to test...but it has a surprise in store. It unlocks all of her dreams of finding a purpose, romance, and glamour in what seems like a game...but it also puts her and her family in real danger.",
+
+    fullDescBy: "—Wilem MacLeod",
+    fullReleaseDate: "October 21, 2022",
+    countryOfOrigin: "United States",
+    Language: "English",
+    filmingLocation: ["Asheville", "North California", "USA"],
+    productionCompanies: [
+      "Amazon Studios",
+      "Big Indie Pictures",
+      "Kilter Films",
+    ],
+
+    cast: [
+      { actorName: "Chloe Grace Moretz", role: "Flynne Fisher" },
+      { actorName: "Gary Carr", role: "Wild Netherton" },
+    ],
+  },
+  {
+    poster: "https://imgs.search.brave.com/6usxrPPEcy2QzghQoMwkE8keSkqmMMegkfBU_C9Hl9o/rs:fit:440:660:1/g:ce/aHR0cHM6Ly93d3cu/dGhlbW92aWVkYi5v/cmcvdC9wL3c0NDBf/YW5kX2g2NjBfZmFj/ZS83VmlRcWxXNVdS/dWxjbkpBZXJjaXpt/QlZ3M3IuanBn",
+    title: "The Peripheral",
+    type: "TV Series",
+    releaseYear: "2022",
+    episodes: 15,
+    tags: ["Darma", "Mystery", "Sci-Fi"],
+    smallDesc:
+      "Set in the future when technology has subtly altered society, a woman discovers a secret connection to an alternate reality as well as a dark future of her own.",
+    creator: "Scott B. Smith",
+    cast: ["Chloe Grace Moretz", "Gary Carr", "Jack Reynor"],
+    rating: "4.5 / 5",
+
+    fullDesc:
+      "Stuck in a small Appalachian town, a young woman's only escape from the daily grind is playing advanced video games. She is such a good player that a company sends her a new video game system to test...but it has a surprise in store. It unlocks all of her dreams of finding a purpose, romance, and glamour in what seems like a game...but it also puts her and her family in real danger.",
+
+    fullDescBy: "—Wilem MacLeod",
+    fullReleaseDate: "October 21, 2022",
+    countryOfOrigin: "United States",
+    Language: "English",
+    filmingLocation: ["Asheville", "North California", "USA"],
+    productionCompanies: [
+      "Amazon Studios",
+      "Big Indie Pictures",
+      "Kilter Films",
+    ],
+
+    cast: [
+      { actorName: "Chloe Grace Moretz", role: "Flynne Fisher" },
+      { actorName: "Gary Carr", role: "Wild Netherton" },
+    ],
+  },
+  {
+    poster: "https://imgs.search.brave.com/6usxrPPEcy2QzghQoMwkE8keSkqmMMegkfBU_C9Hl9o/rs:fit:440:660:1/g:ce/aHR0cHM6Ly93d3cu/dGhlbW92aWVkYi5v/cmcvdC9wL3c0NDBf/YW5kX2g2NjBfZmFj/ZS83VmlRcWxXNVdS/dWxjbkpBZXJjaXpt/QlZ3M3IuanBn",
+    title: "The Peripheral",
+    type: "TV Series",
+    releaseYear: "2022",
+    episodes: 15,
+    tags: ["Darma", "Mystery", "Sci-Fi"],
+    smallDesc:
+      "Set in the future when technology has subtly altered society, a woman discovers a secret connection to an alternate reality as well as a dark future of her own.",
+    creator: "Scott B. Smith",
+    cast: ["Chloe Grace Moretz", "Gary Carr", "Jack Reynor"],
+    rating: "4.5 / 5",
+
+    fullDesc:
+      "Stuck in a small Appalachian town, a young woman's only escape from the daily grind is playing advanced video games. She is such a good player that a company sends her a new video game system to test...but it has a surprise in store. It unlocks all of her dreams of finding a purpose, romance, and glamour in what seems like a game...but it also puts her and her family in real danger.",
+
+    fullDescBy: "—Wilem MacLeod",
+    fullReleaseDate: "October 21, 2022",
+    countryOfOrigin: "United States",
+    Language: "English",
+    filmingLocation: ["Asheville", "North California", "USA"],
+    productionCompanies: [
+      "Amazon Studios",
+      "Big Indie Pictures",
+      "Kilter Films",
+    ],
+
+    cast: [
+      { actorName: "Chloe Grace Moretz", role: "Flynne Fisher" },
+      { actorName: "Gary Carr", role: "Wild Netherton" },
+    ],
+  },
+  {
+    poster: "https://imgs.search.brave.com/6usxrPPEcy2QzghQoMwkE8keSkqmMMegkfBU_C9Hl9o/rs:fit:440:660:1/g:ce/aHR0cHM6Ly93d3cu/dGhlbW92aWVkYi5v/cmcvdC9wL3c0NDBf/YW5kX2g2NjBfZmFj/ZS83VmlRcWxXNVdS/dWxjbkpBZXJjaXpt/QlZ3M3IuanBn",
+    title: "The Peripheral",
+    type: "TV Series",
+    releaseYear: "2022",
+    episodes: 15,
+    tags: ["Darma", "Mystery", "Sci-Fi"],
+    smallDesc:
+      "Set in the future when technology has subtly altered society, a woman discovers a secret connection to an alternate reality as well as a dark future of her own.",
+    creator: "Scott B. Smith",
+    cast: ["Chloe Grace Moretz", "Gary Carr", "Jack Reynor"],
+    rating: "4.5 / 5",
+
+    fullDesc:
+      "Stuck in a small Appalachian town, a young woman's only escape from the daily grind is playing advanced video games. She is such a good player that a company sends her a new video game system to test...but it has a surprise in store. It unlocks all of her dreams of finding a purpose, romance, and glamour in what seems like a game...but it also puts her and her family in real danger.",
+
+    fullDescBy: "—Wilem MacLeod",
+    fullReleaseDate: "October 21, 2022",
+    countryOfOrigin: "United States",
+    Language: "English",
+    filmingLocation: ["Asheville", "North California", "USA"],
+    productionCompanies: [
+      "Amazon Studios",
+      "Big Indie Pictures",
+      "Kilter Films",
+    ],
+
+    cast: [
+      { actorName: "Chloe Grace Moretz", role: "Flynne Fisher" },
+      { actorName: "Gary Carr", role: "Wild Netherton" },
+    ],
+  }, {
+    poster: "https://imgs.search.brave.com/6usxrPPEcy2QzghQoMwkE8keSkqmMMegkfBU_C9Hl9o/rs:fit:440:660:1/g:ce/aHR0cHM6Ly93d3cu/dGhlbW92aWVkYi5v/cmcvdC9wL3c0NDBf/YW5kX2g2NjBfZmFj/ZS83VmlRcWxXNVdS/dWxjbkpBZXJjaXpt/QlZ3M3IuanBn",
+    title: "The Peripheral",
+    type: "TV Series",
+    releaseYear: "2022",
+    episodes: 15,
+    tags: ["Darma", "Mystery", "Sci-Fi"],
+    smallDesc:
+      "Set in the future when technology has subtly altered society, a woman discovers a secret connection to an alternate reality as well as a dark future of her own.",
+    creator: "Scott B. Smith",
+    cast: ["Chloe Grace Moretz", "Gary Carr", "Jack Reynor"],
+    rating: "4.5 / 5",
+
+    fullDesc:
+      "Stuck in a small Appalachian town, a young woman's only escape from the daily grind is playing advanced video games. She is such a good player that a company sends her a new video game system to test...but it has a surprise in store. It unlocks all of her dreams of finding a purpose, romance, and glamour in what seems like a game...but it also puts her and her family in real danger.",
+
+    fullDescBy: "—Wilem MacLeod",
+    fullReleaseDate: "October 21, 2022",
+    countryOfOrigin: "United States",
+    Language: "English",
+    filmingLocation: ["Asheville", "North California", "USA"],
+    productionCompanies: [
+      "Amazon Studios",
+      "Big Indie Pictures",
+      "Kilter Films",
+    ],
+
+    cast: [
+      { actorName: "Chloe Grace Moretz", role: "Flynne Fisher" },
+      { actorName: "Gary Carr", role: "Wild Netherton" },
+    ],
+  },
+  {
+    poster: "https://imgs.search.brave.com/6usxrPPEcy2QzghQoMwkE8keSkqmMMegkfBU_C9Hl9o/rs:fit:440:660:1/g:ce/aHR0cHM6Ly93d3cu/dGhlbW92aWVkYi5v/cmcvdC9wL3c0NDBf/YW5kX2g2NjBfZmFj/ZS83VmlRcWxXNVdS/dWxjbkpBZXJjaXpt/QlZ3M3IuanBn",
+    title: "The Peripheral",
+    type: "TV Series",
+    releaseYear: "2022",
+    episodes: 15,
+    tags: ["Darma", "Mystery", "Sci-Fi"],
+    smallDesc:
+      "Set in the future when technology has subtly altered society, a woman discovers a secret connection to an alternate reality as well as a dark future of her own.",
+    creator: "Scott B. Smith",
+    cast: ["Chloe Grace Moretz", "Gary Carr", "Jack Reynor"],
+    rating: "4.5 / 5",
+
+    fullDesc:
+      "Stuck in a small Appalachian town, a young woman's only escape from the daily grind is playing advanced video games. She is such a good player that a company sends her a new video game system to test...but it has a surprise in store. It unlocks all of her dreams of finding a purpose, romance, and glamour in what seems like a game...but it also puts her and her family in real danger.",
+
+    fullDescBy: "—Wilem MacLeod",
+    fullReleaseDate: "October 21, 2022",
+    countryOfOrigin: "United States",
+    Language: "English",
+    filmingLocation: ["Asheville", "North California", "USA"],
+    productionCompanies: [
+      "Amazon Studios",
+      "Big Indie Pictures",
+      "Kilter Films",
+    ],
+
+    cast: [
+      { actorName: "Chloe Grace Moretz", role: "Flynne Fisher" },
+      { actorName: "Gary Carr", role: "Wild Netherton" },
+    ],
+  },
+];
+
+export default Info
